b1da4037cd065ca6405a17ae2ca4cf6daa048a5f
issamv3 <gbot2613@gmail.com>
2025-07-12 12:14:21 +0000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-const PORT = process.env.PORT || 3000;
+const PORT =  7860;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on http://localhost:${PORT}`);
